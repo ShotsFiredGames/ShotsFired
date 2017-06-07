54994c60ccd8ec69c927da3ac16372522f94995c
hewittm1 <hewittm1@gmatc.matc.edu>
2017-06-07 14:50:31 -0500

    Scion Arena
    White box of the first level design. Includes two sets of teleporters
    along with various jumppads.

---
+++ Assets/Game/Scripts/GameplayScripts/Teleporter.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+    public Transform otherTeleporter;
+    // Use this for initialization
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+            other.transform.position = new Vector3(otherTeleporter.transform.position.x, otherTeleporter.transform.position.y, otherTeleporter.transform.position.z + 1);
+    }
+}
