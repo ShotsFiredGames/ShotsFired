3dacb171df27744fcbb5f8d5741321a32f158ece
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-06-18 12:49:34 -0500

    Added Random Gun and Huge Load

---
+++ Assets/Game/Scripts/EventScripts/GiftedGun.cs
@@ -0,0 +1,21 @@
+﻿//using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiftedGun : AddOn
+{
+    public List<string> possibleGunNames;
+    PlayerManager[] allPlayers;
+
+    public override void StartAddOn()
+    {
+        allPlayers = PlayerWrangler.GetUnorderedPlayers();
+
+        foreach (PlayerManager player in allPlayers)
+        {
+            player.CmdDisarm();
+            player.CmdWeaponPickedUp(possibleGunNames[Random.Range(0, possibleGunNames.Count)]);
+        }
+    }
+}
