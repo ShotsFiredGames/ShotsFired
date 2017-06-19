b6fba39dd6b6ac69668463263f2cbe37faca0280
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-06-19 12:54:43 -0500

    first part of ctf

---
+++ Assets/Game/Scripts/EventScripts/CaptureTheFlag.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureTheFlag : GameEvent
+{
+    public GameObject[] objectsToSetActive;
+    public GameObject flag;
+    public GameObject flagSpawnpoint;
+    public GameObject[] endPoints;
+    public float eventLength;
+
+    Coroutine captureTheFlag;
+
+    public override void StartEvent()
+    {
+        foreach(GameObject go in objectsToSetActive)
+            go.SetActive(false);
+
+        foreach (GameObject go in endPoints)
+            go.SetActive(true);
+
+        flag.GetComponent<Flag>().enabled = true;
+        flag.SetActive(true);
+
+        captureTheFlag = StartCoroutine(EventLength());
+    }
+
+    IEnumerator EventLength()
+    {
+        yield return new WaitForSeconds(eventLength);
+        EndEvent();
+    }
+
+    public override void EndEvent()
+    {
+        foreach (GameObject go in objectsToSetActive)
+            go.SetActive(true);
+
+        foreach (GameObject go in endPoints)
+            go.SetActive(false);
+
+        flag.GetComponent<Flag>().enabled = false;
+        flag.SetActive(false);
+    }
+}
---
+++ Assets/Game/Scripts/EventScripts/Flag.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flag : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Collision"))
+        {
+            //
+        }
+    }
+}
