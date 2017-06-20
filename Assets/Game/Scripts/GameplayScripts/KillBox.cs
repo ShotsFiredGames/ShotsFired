7a4744d1c0b3d04c562b5b9ee43a3aa36ca669ff
Sam <sam@defendtheouterrim.com>
2017-06-20 13:40:07 -0500

    added killbox

---
+++ Assets/Game/Scripts/GameplayScripts/KillBox.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class KillBox : NetworkBehaviour {
+
+	[ServerCallback]
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            other.GetComponent<PlayerHealth>().RpcInstantDeath("Environment", CollisionDetection.CollisionFlag.Back);
+        }
+    }
+}
