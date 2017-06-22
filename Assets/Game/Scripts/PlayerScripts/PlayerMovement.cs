b7a794b2a3923b27d0e47899d3e4db1023e42e1f
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-06-22 12:51:19 -0500

    Check Description
    Reduced Movement speed slightly
    Increased fire frequency slightly for the machine gun
    Decrease the damage of the machine gun slightly
    Increased the rocket speed for the rocket launcher by half
    Fixed a bug where you could not jump on one of the platforms
    increase slow amount of aim assist
    added 4 jumppads on each side of the map
    reduced the speed of the juggernaut pick up

--- Assets/Game/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Game/Scripts/PlayerScripts/PlayerMovement.cs
@@ -35,7 +35,7 @@ public class PlayerMovement : NetworkBehaviour
         _jump = jumpForce;
         playerCamera = GetComponent<PlayerCamera>();
 
-        airSpeed = speed * .55f;
+        airSpeed = speed * .35f;
 	}
 
     public void AimAssist()
