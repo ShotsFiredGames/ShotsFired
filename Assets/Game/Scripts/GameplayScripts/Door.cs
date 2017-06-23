d9d9d250f10f732529418a0149966bca5a055310
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-06-23 10:20:46 -0500

    Added the new level

---
+++ Assets/Game/Scripts/GameplayScripts/Door.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag.Equals("Player"))
+        {
+            print("Open");
+            anim.SetBool("IsOpen", true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag.Equals("Player"))
+        {
+            print("Close");
+            anim.SetBool("IsOpen", false);
+        }
+    }
+}
