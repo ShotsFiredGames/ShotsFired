cdb0a81cb1324d1992a43faad0c3a1aba64f1b5d
Borgeshc <Borgeshc@gmatc.matc.edu>
2017-06-27 13:48:55 -0500

    Finished up bombs away!

---
+++ Assets/Game/Scripts/EventScripts/BombsAway.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BombsAway : AddOn
+{
+    public List<GameObject> bombs;
+    List<GameObject> availibleBombs;
+    int halfOfBombs;
+
+    private void Start()
+    {
+        availibleBombs = new List<GameObject>();
+
+        PopulateList();
+    }
+
+    public override void StartAddOn()
+    {
+        PopulateList();
+        StartCoroutine(SpawnBombs());
+    }
+
+    IEnumerator SpawnBombs()
+    {
+        for(int i = 0; i < bombs.Count; i++)
+        {
+            yield return new WaitForSeconds(.25f);
+            GameObject bombToActivate = NextBomb();
+            bombToActivate.SetActive(true);
+            availibleBombs.Remove(bombToActivate);
+        }
+    }
+
+    GameObject NextBomb()
+    {
+        return bombs[Random.Range(0, availibleBombs.Count)];
+    }
+
+    void PopulateList()
+    {
+        for(int i = 0; i < bombs.Count; i++)
+        {
+            if(!availibleBombs.Contains(bombs[i]))
+            {
+                availibleBombs.Add(bombs[i]);
+            }
+        }
+    }
+}
