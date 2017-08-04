7caa08ca0a036b47e1606be353c00ad9f5911449
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-08-04 12:08:24 -0500

    Added The Start of Control Settings

---
+++ Assets/ControlSetting.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSetting : MonoBehaviour
+{
+    public static Controls controls;
+
+    void Awake()
+    {
+        controls = Controls.CreateWithDefaultBindings();
+    }
+
+    public static Controls GetControls()
+    {
+        if (controls == null)
+            controls = Controls.CreateWithDefaultBindings();
+
+        return controls;
+    }
+
+    public void RebindAControl(string actionName)
+    {
+        foreach (var action in controls.Actions)
+        {
+            if (action.Name.Equals(actionName))
+            {
+                print(action.Name);
+                action.ListenForBinding();
+            }
+        }
+    }
+}
