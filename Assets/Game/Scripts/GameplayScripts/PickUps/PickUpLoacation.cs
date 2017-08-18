2729db0d3d5c47ec9bd795823f3dc73255335f34
PeterHoppers <hoppepj@gmatc.matc.edu>
2017-08-18 10:21:30 -0500

    Bug Fixes Round 5

--- Assets/Game/Scripts/GameplayScripts/PickUps/PickUpLoacation.cs
+++ Assets/Game/Scripts/GameplayScripts/PickUps/PickUpLoacation.cs
@@ -70,7 +70,7 @@ public class PickUpLoacation : Photon.MonoBehaviour
     {
         foreach (GameObject pickup in pickUpTypes)
         {
-            Debug.LogError(gameObject.name + " has a pickup " + activePickUp.name);
+            Debug.LogError(gameObject.name + " has a pickup " + pickup.name);
         }
 
         activePickUp = Instantiate(pickUpTypes[pickUp], transform.position + spawnOffset, Quaternion.identity);
